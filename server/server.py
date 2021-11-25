74cc47dd5a3d6c86fd32ba425ab57a027bc192f3
Tetre2 <mattias.retteli@gmail.com>
2021-11-25 18:42:37 +0100

    nicer implementation for forwarding entries to the leader

--- server/server.py
+++ server/server.py
@@ -136,7 +136,6 @@ try:
             new_entry = request.forms.get('entry')
 
             if int(my_leader) != node_id: # if this is not the leader forward the request to the leader
-                print("dosadlkakdjslak")
                 thread = Thread(target=contact_vessel, args=(
                     vessel_list[my_leader], "/board", {'entry': new_entry}, 'POST'))
                 thread.daemon = True
