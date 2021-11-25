92684959ec351fc1e6cae777d5ec36f22790a49b
Tetre2 <mattias.retteli@gmail.com>
2021-11-25 18:00:57 +0100

    05

--- server/server.py
+++ server/server.py
@@ -22,6 +22,9 @@ try:
     # board stores all message on the system
     board = {0: "Welcome to Distributed Systems Course"}
 
+    has_leader = False
+    my_leader = "-1"
+
     # ------------------------------------------------------------------------------------------------------
     # BOARD FUNCTIONS
     # You will probably need to modify them
@@ -68,9 +71,6 @@ try:
     # a single example (index) for get, and one for post
     # ------------------------------------------------------------------------------------------------------
     # No need to modify this
-
-    has_leader = False
-    my_leader = "-1"
 
     @app.route('/')
     def index():
@@ -131,25 +131,28 @@ try:
     def client_add_received():
         '''Adds a new element to the board
         Called directly when a user is doing a POST request on /board'''
-        global board, node_id
-        try:
-            new_entry = request.forms.get('entry')
-            # When generating an ID for a new element we random a int 0-1000, if it exists in the board generate a now id
-            element_id = random.randint(0, 1000)
-            while element_id in board:
+        global board, node_id, my_leader
+        if int(my_leader) == node_id: # if this is the leader change the board otherwise forward the request to the leader
+            try:
+                new_entry = request.forms.get('entry')
+                # When generating an ID for a new element we random a int 0-1000, if it exists in the board generate a now id
                 element_id = random.randint(0, 1000)
-                
-            add_new_element_to_store(element_id, new_entry)
+                while element_id in board:
+                    element_id = random.randint(0, 1000)
+                    
+                add_new_element_to_store(element_id, new_entry)
 
-            # Then we propagate the new element
-            thread = Thread(target=propagate_to_vessels, args=(
-                '/propagate/ADD/' + str(element_id), {'entry': new_entry}, 'POST'))
-            thread.daemon = True
-            thread.start()
-            return True
-        except Exception as e:
-            print (e)
-        return False
+                # Then we propagate the new element
+                thread = Thread(target=propagate_to_vessels, args=(
+                    '/propagate/ADD/' + str(element_id), {'entry': new_entry}, 'POST'))
+                thread.daemon = True
+                thread.start()
+                return True
+            except Exception as e:
+                print (e)
+            return False
+        else:
+            contact_vessel(vessel_list[my_leader], "/board", request.forms, "POST")
 
     @app.post('/board/<element_id:int>/')
     def client_action_received(element_id):
