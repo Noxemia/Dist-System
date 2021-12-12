9710b644d79d7cb36bb76f9854486a62cd59f89b
Aaron Sandgren <aaron.sandgren@live.se>
2021-12-12 18:18:21 +0100

    test seq gen13

--- server/server.py
+++ server/server.py
@@ -10,6 +10,7 @@ import sys
 import time
 import json
 import argparse
+import random
 from threading import Thread
 
 from bottle import Bottle, run, request, template
@@ -20,6 +21,7 @@ try:
 
     # board stores all message on the system
     board = {0: "Welcome to Distributed Systems Course"}
+    seq_board = {0:0}
 
     sequenser = 1
 
@@ -38,7 +40,7 @@ try:
                 board[int(entry_sequence)] = element
                 success = True
         except Exception as e:
-            print (e)
+            print(e)
         return success
 
     # This function will update an existing element in the board dictionary
@@ -49,7 +51,7 @@ try:
             board[int(entry_sequence)] = modified_element
             success = True
         except Exception as e:
-            print (e)
+            print(e)
         return success
 
     # This function will remove an element from the board dictionary
@@ -60,7 +62,7 @@ try:
             board.pop(int(entry_sequence))
             success = True
         except Exception as e:
-            print (e)
+            print(e)
         return success
 
     # ------------------------------------------------------------------------------------------------------
@@ -72,7 +74,7 @@ try:
     @app.route('/')
     def index():
         global board, node_id
-        if int(node_id) == 1:
+        if int(node_id) == 2:
             get_consistency()
         return template('server/index.tpl', board_title='Vessel {}'.format(node_id),
                         board_dict=sorted({"0": board, }.iteritems()), members_name_string='YOUR NAME')
@@ -80,15 +82,16 @@ try:
     @app.get('/board')
     def get_board():
         global board, node_id
-        print (board)
+        print(board)
         return template('server/boardcontents_template.tpl', board_title='Vessel {}'.format(node_id), board_dict=sorted(board.iteritems()))
 
     @app.get('/get_board')
     def get_board():
         global board
-        return json.dumps({"board":board})
+        return json.dumps({"board": board, 'seq_board': seq_board})
 
     seq = 0
+
     @app.get('/sequence')
     def get_sequence():
         global seq, node_id
@@ -118,79 +121,79 @@ try:
                 seq = res.json().get('seq')
                 print("Sequence: ", seq)
             else:
-                print("Sequencer failed!!!")    
+                print("Sequencer failed!!!")
 
-
-            if len(board) != 0:
-                element_id = int(max(board.keys()) + 1)
-            else:
-                element_id = 0
+            element_id = str(random.randint(0, 1000))
+            while element_id in board:
+                element_id = str(random.randint(0, 1000))
             add_new_element_to_store(element_id, new_entry)
 
             # Then we propagate the new element
-            thread = Thread(target=propagate_to_vessels, args=(
+            thread=Thread(target=propagate_to_vessels, args=(
                 '/propagate/ADD/' + str(element_id), {'entry': new_entry, 'seq': seq}, 'POST'))
-            thread.daemon = True
+            thread.daemon=True
             thread.start()
             return True
         except Exception as e:
-            print (e)
+            print(e)
         return False
 
-    @app.post('/board/<element_id:int>/')
+    @ app.post('/board/<element_id:int>/')
     def client_action_received(element_id):
         global board, node_id
 
-        print ("You receive an element")
-        print ("id is "), node_id
+        print("You receive an element")
+        print("id is "), node_id
         # Get the entry from the HTTP body
-        entry = request.forms.get('entry')
+        entry=request.forms.get('entry')
 
-        delete_option = request.forms.get('delete')
+        delete_option=request.forms.get('delete')
         # 0 = modify, 1 = delete
 
-        print ("the delete option is "), delete_option
+        print("the delete option is "), delete_option
 
         # If a post request is received on board/elementid with the request data of delete = 0 we call the modify function with
         # the data received.
         if delete_option == "0":
             modify_element_in_store(element_id, entry, True)
-            thread = Thread(target=propagate_to_vessels, args=(
+            thread=Thread(target=propagate_to_vessels, args=(
                 '/propagate/MODIFY/' + str(element_id), {'entry': entry}, 'POST'))
-            thread.daemon = True
+            thread.daemon=True
             thread.start()
 
         # If a post request is received on board/elementid with the request data of delete = 1 we call the delete function with
         # the data received.
         if delete_option == "1":
             delete_element_from_store(element_id, True)
-            thread = Thread(target=propagate_to_vessels, args=(
+            thread=Thread(target=propagate_to_vessels, args=(
                 '/propagate/DELETE/' + str(element_id), {'entry': entry}, 'POST'))
-            thread.daemon = True
+            thread.daemon=True
             thread.start()
 
 
     # With this function you handle requests from other nodes like add modify or delete
 
-    @app.post('/propagate/<action>/<element_id>')
+    @ app.post('/propagate/<action>/<element_id>')
     def propagation_received(action, element_id):
         # get entry from http body
-        entry = request.forms.get('entry')
-        seq = request.forms.get('seq')
-        print ("the action is"), action
+        entry=request.forms.get('entry')
+        seq=request.forms.get('seq')
+        print("the action is"), action
 
         # Handle requests
         # for example action == "ADD":
         if action == "ADD":
-            print("Recivied ADD with seq:", seq)
+            seq_board[element_id] = seq
             add_new_element_to_store(element_id, entry, True)
 
         # Modify the board entry
         if action == "MODIFY":
+            seq_board[element_id] = seq
             modify_element_in_store(element_id, entry, True)
 
         # Delete the entry from the board
         if action == "DELETE":
+            seq_board[element_id] = -1
             delete_element_from_store(element_id, True)
 
     # ------------------------------------------------------------------------------------------------------
@@ -199,21 +202,21 @@ try:
 
     def contact_vessel(vessel_ip, path, payload=None, req='POST'):
         # Try to contact another server (vessel) through a POST or GET, once
-        success = False
+        success=False
         try:
             if 'POST' in req:
-                res = requests.post(
+                res=requests.post(
                     'http://{}{}'.format(vessel_ip, path), data=payload)
             elif 'GET' in req:
-                res = requests.get('http://{}{}'.format(vessel_ip, path))
+                res=requests.get('http://{}{}'.format(vessel_ip, path))
             else:
-                print ('Non implemented feature!')
+                print('Non implemented feature!')
             # result is in res.text or res.json()
             print(res.text)
             if res.status_code == 200:
-                success = True
+                success=True
         except Exception as e:
-            print (e)
+            print(e)
         return success
 
     def propagate_to_vessels(path, payload=None, req='POST'):
@@ -221,25 +224,28 @@ try:
 
         for vessel_id, vessel_ip in vessel_list.items():
             if int(vessel_id) != node_id:  # don't propagate to yourself
-                success = contact_vessel(vessel_ip, path, payload, req)
+                success=contact_vessel(vessel_ip, path, payload, req)
                 if not success:
                     print("\n\nCould not contact vessel {}\n\n".format(vessel_id))
 
     def get_consistency():
         global board,  vessel_list, node_id
-        return None
-        suc = []
+
+        boards=[]
+        seq_boards = []
 
         for vessel_id, vessel_ip in vessel_list.items():
             if int(vessel_id) != node_id:  # don't propagate to yourself
-                res = requests.get('http://{}/get_board'.format(vessel_ip))
+                res=requests.get('http://{}/get_board'.format(vessel_ip))
                 if res.status_code == 200:
-                    suc.append(res)
-        
-        boards = []
-        for respons in suc:
-            resjson = json.loads(respons.json())
-            boards.append(resjson.board)
+                    boards.append(res.json().get('board'))
+                    seq_boards.append(res.json().get('seq_board'))
+
+        for i in range(len(boards)):
+            print(i)
+            print(boards[i])
+            print(seq_boards[i])
+            print("--------------")
 
     # ------------------------------------------------------------------------------------------------------
     # EXECUTION
@@ -248,24 +254,24 @@ try:
     def main():
         global vessel_list, node_id, app
 
-        port = 80
-        parser = argparse.ArgumentParser(
+        port=80
+        parser=argparse.ArgumentParser(
             description='Your own implementation of the distributed blackboard')
         parser.add_argument('--id', nargs='?', dest='nid',
                             default=1, type=int, help='This server ID')
         parser.add_argument('--vessels', nargs='?', dest='nbv', default=1,
                             type=int, help='The total number of vessels present in the system')
-        args = parser.parse_args()
-        node_id = args.nid
-        vessel_list = dict()
+        args=parser.parse_args()
+        node_id=args.nid
+        vessel_list=dict()
         # We need to write the other vessels IP, based on the knowledge of their number
         for i in range(1, args.nbv+1):
-            vessel_list[str(i)] = '10.1.0.{}'.format(str(i))
+            vessel_list[str(i)]='10.1.0.{}'.format(str(i))
 
         try:
             run(app, host=vessel_list[str(node_id)], port=port)
         except Exception as e:
-            print (e)
+            print(e)
     # ------------------------------------------------------------------------------------------------------
     if __name__ == '__main__':
         main()
