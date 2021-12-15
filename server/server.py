cfe74dbb81b0e88c1cc1448b5a71e0879f5b1bff
Aaron Sandgren <aaron.sandgren@live.se>
2021-12-15 14:13:23 +0100

    finalday3

--- server/server.py
+++ server/server.py
@@ -242,8 +242,8 @@ try:
             if int(vessel_id) != node_id:  # don't propagate to yourself
                 res=requests.get('http://{}/get_board'.format(vessel_ip))
                 if res.status_code == 200:
-                    boards.append(json.loads(res.json().get('board')))
-                    seq_boards.append(json.loads(res.json().get('seq_board')))
+                    boards.append(json.load(res.json().get('board')))
+                    seq_boards.append(json.load(res.json().get('seq_board')))
 
         print(boards)
         print(seq_boards)
